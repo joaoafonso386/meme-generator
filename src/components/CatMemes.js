caa84c366cd463f49596f0d933a3339bb6ccb240
João Afonso <joaoafonso386@gmail.com>
2021-10-26 22:39:50 +0100

    Added final CSS to correctly display the image grid

--- src/components/CatMemes.js
+++ src/components/CatMemes.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./CatMeme.css";
 
 const CatMemes = ({ memes }) => {
   const mapMemes = memes.map((meme) => {
     return meme.name.toLowerCase().includes("cat") ? (
       <div className="four wide column" key={meme.id}>
-        <div className="ui segment">
+        <div className="ui container-img segment">
           <h3>{meme.name}</h3>
-          <img className="ui medium rounded image" src={meme.url} alt="" />
+          <img className="ui rounded image" src={meme.url} alt="" />
         </div>
       </div>
     ) : (
@@ -16,7 +17,7 @@ const CatMemes = ({ memes }) => {
 
   return (
     <div>
-      <h1>Excelent Cat Memes</h1>
+      <h1 className="cat-title">Excelent Cat Memes</h1>
       <div className="ui grid">{mapMemes}</div>
     </div>
   );
