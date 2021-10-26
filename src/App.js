7c1cc7c040879364d99b2e8a6ac12b137c97b078
João Afonso <joaoafonso386@gmail.com>
2021-10-26 14:36:45 +0100

    Starting point

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import SearchInput from "./SearchInput";
 
 function App() {
+  const [imgsArray, setImagesArray] = useState([]);
+  const [img, setImage] = useState({
+    name: "",
+    url: "",
+  });
+
+  const generateImg = () => {
+    const randomMeme = imgsArray[Math.floor(Math.random() * imgsArray.length)];
+    const { name, url } = randomMeme;
+    setImage({
+      name: name,
+      url: url,
+    });
+  };
+
+  useEffect(() => {
+    axios.get("https://api.imgflip.com/get_memes").then((response) => {
+      const { memes } = response.data.data;
+      const { name, url } = memes[Math.floor(Math.random() * imgsArray.length)];
+      setImagesArray(memes);
+      setImage({
+        name: name,
+        url: url,
+      });
+    });
+  }, [imgsArray.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>{img.name}</h1>
+        <img width="400px" src={img.url} alt="" />
+      </div>
+      <button onClick={generateImg}>Generate</button>
+      <SearchInput setImage={setImage} />
     </div>
   );
 }
